b60f32708df139d7294f3d790572f57a77c521c3
Konstantin Orlov <genius4666@gmail.com>
2022-09-05 13:13:10 +0300

    basis: example, .env, logrus

---
+++ cmd/bot/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"os"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"github.com/joho/godotenv"
+	"github.com/sirupsen/logrus"
+)
+
+func main() {
+	logrus.SetFormatter(new(logrus.JSONFormatter))
+
+	if err := godotenv.Load(); err != nil {
+		logrus.Println(err.Error())
+		return
+	}
+
+	bot, err := tgbotapi.NewBotAPI(os.Getenv("TOKEN"))
+	if err != nil {
+		logrus.Panic(err)
+	}
+
+	bot.Debug = true
+
+	logrus.Printf("Authorized on account %s", bot.Self.UserName)
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates := bot.GetUpdatesChan(u)
+
+	for update := range updates {
+		if update.Message != nil { // If we got a message
+			logrus.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
+
+			bot.Send(msg)
+		}
+	}
+}
---
+++ go.mod
@@ -0,0 +1,13 @@
+module github.com/genius321/pocketer-telegram-bot
+
+go 1.19
+
+require (
+	github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.5.1
+	github.com/sirupsen/logrus v1.9.0
+)
+
+require (
+	github.com/joho/godotenv v1.4.0
+	golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
+)
